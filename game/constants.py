09a28dd618065265ec57233cfce52dba97bf156f
Rodrigo Aiosa <32776696+RodrigoAiosa@users.noreply.github.com>
2026-07-28 22:46:36 -0300

    Add selectable board sizes for game difficulty
    Added selectable board sizes for difficulty levels.

--- game/constants.py
+++ game/constants.py
@@ -3,6 +3,9 @@
 # Tamanho do tabuleiro (SIZE x SIZE)
 SIZE = 4
 
+# Tamanhos de tabuleiro selecionáveis (dificuldade)
+BOARD_SIZES = [4, 5, 6]
+
 # Probabilidade de uma peça nova nascer no 2º nível (valor 4) em vez do 1º (valor 2)
 NEW_TILE_FOUR_CHANCE = 0.1
 
